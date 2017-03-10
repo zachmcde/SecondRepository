3f2d67c9a341b202e6a4995f23c8dfb3d79ad870
Zach McDermott <zach.mcdermott@yahoo.com>
2017-03-10 11:07:32 -0800

    Initial Commit

---
+++ SecondRepository/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecondRepository
+//
+//  Created by Zach McDermott on 3/10/17.
+//  Copyright © 2017 Zach McDermott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
